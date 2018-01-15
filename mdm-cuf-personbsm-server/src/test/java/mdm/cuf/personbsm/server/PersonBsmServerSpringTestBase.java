a364f51343811978d7de61230490b98f706a1aec
Jon Shrader <jshrader@technility.com>
2018-01-15 14:26:47 -0500

    removing redundant profile activation

--- mdm-cuf-personbsm-server/src/test/java/mdm/cuf/personbsm/server/PersonBsmServerSpringTestBase.java
+++ mdm-cuf-personbsm-server/src/test/java/mdm/cuf/personbsm/server/PersonBsmServerSpringTestBase.java
@@ -1,17 +1,14 @@
 package mdm.cuf.personbsm.server;
 
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import mdm.cuf.core.server.AbstractMdmCufCoreServerSpringTest;
-import mdm.cuf.core.server.MdmCufCoreServerProfiles;
 
 /**
  * This is the base test class for Spring tests within MDM CUF utilapp Server
  * 
  * @author jshrader
  */
-@ActiveProfiles(MdmCufCoreServerProfiles.MODE_UTIL_MICROSERVICE)
 @Import(PersonBsmServerConfig.class)
 public abstract class PersonBsmServerSpringTestBase extends AbstractMdmCufCoreServerSpringTest {
     
